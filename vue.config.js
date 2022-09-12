28102b347991808d0aa328212595c13794ae4bf5
mark <a@b.com>
2022-09-12 16:21:07 +0800

    change path

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
   outputDir: "docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/print_ct_route/" : "/",
 });
